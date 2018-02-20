e9376bf6f7bb16ecbaa2018bb5790c5cf8d81fbb
Akilan Rajendran <akilan.irs@gmail.com>
2018-02-20 18:25:48 +0530

    first commit

---
+++ app/src/main/java/recyclerapp/ricebeerinc/com/recyclerview/Contact.java
@@ -0,0 +1,26 @@
+package recyclerapp.ricebeerinc.com.recyclerview;
+
+/**
+ * Created by dell on 20-02-2018.
+ */
+
+public class Contact {
+    private String email;
+    private String phone;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ app/src/main/java/recyclerapp/ricebeerinc/com/recyclerview/OnLoadMoreListener.java
@@ -0,0 +1,9 @@
+package recyclerapp.ricebeerinc.com.recyclerview;
+
+/**
+ * Created by dell on 20-02-2018.
+ */
+
+public interface OnLoadMoreListener {
+    void onLoadMore();
+}
